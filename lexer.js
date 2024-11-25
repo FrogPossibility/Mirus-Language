adfecb73a4f2aa60357df7d87217222c2dd70cfa
ProFrogCreator <profrogcreator@gmail.com>
2024-11-25 17:06:33 +0100

    first commit

---
+++ lexer.js
@@ -0,0 +1,12 @@
+const moo = require("moo");
+
+module.exports = moo.compile({
+    whitespace:      /[ \t]+/,
+    number:          { match: /0|[1-9][0-9]*/, value: Number },
+    string:          /"(?:\\["\\]|[^\n"\\])*"/,
+    left_paren:      '(',
+    right_paren:     ')',
+    assignment_op:   "=",
+    identifier:      /[a-zA-Z_][a-zA-Z0-9_]*/,
+    newline: { match: /\r?\n/, lineBreaks: true },
+});
